sparky/rsget.pl

commit 5221a7fdc55875c657ee7289b6a64f120d82d45b

- simplified info() and bestinfo()
git-svn-id: http://svn.pld-linux.org/svn/toys/rsget.pl@11677 11da6c6d-24c7-0310-97e0-b6d17e038e62

RSGet/Get.pm
@@ -380,11 +380,8 @@ sub bestinfo
 {
 	my $self = shift;
 	my $o = $self->{_opts};
-	my $i = $self->{info};
 
 	my $bestname = $o->{fname}
-		|| $i->{name} || $i->{iname}
-		|| $i->{aname} || $i->{ainame}
 		|| $o->{name} || $o->{iname}
 		|| $o->{aname} || $o->{ainame};
 	unless ( $bestname ) {
@@ -396,7 +393,6 @@ sub bestinfo
 	$self->{_name} = $bestname;
 
 	my $bestsize = $o->{fsize}
-		|| $i->{size} || $i->{asize}
 		|| $o->{size} || $o->{asize}
 		|| "?";
 	$self->{bestsize} = $bestsize;
@@ -406,10 +402,10 @@ sub info
 {
 	my $self = shift;
 	my %info = @_;
-	$info{asize} =~ s/ //g if $info{asize};
+	$info{asize} =~ s/\s+//g if $info{asize};
 	RSGet::FileList::save( $self->{_uri}, options => \%info );
 
-	$self->{info} = \%info;
+	@{$self->{_opts}->{ keys %info }} = values %info;
 	$self->bestinfo();
 
 	return 0 unless $self->{_cmd} eq "check";

commit 27b0ea8325b4be5d9bc217d97e1a25cb59c71eff

- fixed breakage introduced in r11677
git-svn-id: http://svn.pld-linux.org/svn/toys/rsget.pl@11678 11da6c6d-24c7-0310-97e0-b6d17e038e62

RSGet/Get.pm
@@ -405,7 +405,7 @@ sub info
 	$info{asize} =~ s/\s+//g if $info{asize};
 	RSGet::FileList::save( $self->{_uri}, options => \%info );
 
-	@{$self->{_opts}->{ keys %info }} = values %info;
+	hadd( $self->{_opts}, %info );
 	$self->bestinfo();
 
 	return 0 unless $self->{_cmd} eq "check";